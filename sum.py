40656d73f6c4d5ef9843756a6d32eaa47f0a5191
python172 <62088371+python172@users.noreply.github.com>
2020-03-12 11:37:53 +0700

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,17 @@
+##Summation up to infinity
+sum1 = 0
+sum2 = 0
+#define the value of realtive tolerence
+RelTol = 1.e-1000
+for n in range(1,100001):
+    if (n==1):
+        sum1 = 1/((n**2)*((n+1)**2) *((n+2)**2))
+    elif (n > 1):
+        sum2 = sum1 + 1/(((n)**2)*((n+1)**2)*((n+2)**2))
+        RelErr = abs(sum2-sum1)/sum1
+        if(RelErr <= RelTol):
+            break
+        else:
+            sum1 = sum2
+
+print("Term: ", n,  "Your summation is ", sum2)
